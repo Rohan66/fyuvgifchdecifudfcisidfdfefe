7cadf74f8821516781080d301a73d47a20c37a02
Rohan66 <78190074+Rohan66@users.noreply.github.com>
2021-09-20 10:50:32 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+localStorage.getItem(Welcome);
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+<div></div>
+//End code
+});});}
+getData();
+function rediectToRoomName(){
+    localStorage.getItem.innerHTML
+}
+
